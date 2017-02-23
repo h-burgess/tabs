138797d3836b65d138027e9008386ac1279434b7
Harry J Burgess <hburgess.dev@gmail.com>
2017-02-23 22:54:09 +0000

    Better styling and cleaning up code

--- js/main.js
+++ js/main.js
@@ -1,12 +1,3 @@
-// Nav dropdown animation
-$(document).ready(function () {
-    $(".menu-text").click(function () {
-        $(".top-layer-nav-list").slideToggle(200);
-        $('#top-nav > ul li ul').hide();
-    });
-    // This animated the toggle.
-    if ($('.target').is(':visible')) {}
-});
 $('#top-nav li ul').hide().removeClass('fallback');
 $('#top-nav > .top-layer-nav-list > li').click(function () {
     $('#top-nav li ul').not($(this).children("ul").slideToggle(200)).hide();
