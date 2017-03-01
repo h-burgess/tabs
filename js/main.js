bcecf7535dc8ed19c72ee35c2cc8dbaeb2fe1e3e
Harry J Burgess <hburgess.dev@gmail.com>
2017-03-01 22:31:48 +0000

    Cleaning code

--- js/main.js
+++
@@ -1,6 +0,0 @@
-$('#top-nav li ul').hide().removeClass('fallback');
-$('#top-nav > .top-layer-nav-list > li').click(function () {
-    $('#top-nav li ul').not($(this).children("ul").slideToggle(200)).hide();
-    $('#top-nav li').not($(this).toggleClass('extended')).removeClass('extended');
-});
-
